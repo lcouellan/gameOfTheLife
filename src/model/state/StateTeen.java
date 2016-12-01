a341c8ef484345a310a8c2c9ebdcc0bd86499bb4
Raphael-Erfani <raphael.erfani@gmail.com>
2016-12-01 16:48:44 +0100

    Mise en place du StateChild

--- src/model/state/StateTeen.java
+++ src/model/state/StateTeen.java
@@ -3,7 +3,7 @@ package model.state;
 import model.entity.Fish;
 
 /**
- * La classe <b>StateChild</b> est une classe qui permet la gestion des mouvements de nos poissons avec un état "jeune adulte".
+ * La classe <b>StateChild</b> est une classe qui permet la gestion des mouvements de nos requins avec un état "jeune adulte".
  * 
  * @author Raphaël Erfani, Lénaïc Couëllan
  * @version 1.0
@@ -12,8 +12,18 @@ import model.entity.Fish;
 public class StateTeen extends StateFish{
 
 	@Override
+	/**
+	 * Méthode qui permet de faire un déplacement plus intelligent que celui du statut "enfant".
+	 * Ce déplacement simule donc le déplacement d'un requin "jeune adulte". Ce dernier se déplace sur une case adjacente
+	 * lorsque cette dernière contient une sardine. Un requin "jeune adulte" à donc des déplacements plus menaçant qu'un requin "enfant"
+	 * Si aucunes sardine ne se trouvent sur les cases adjacentes, on reste sur un mouvement aléatoire.
+	 */
 	public void move(Fish fish) {
 		// TODO Auto-generated method stub
+		// Si les cellules adjacentes sont différentes de null, utiliser le StateChild
+		// Sinon se diriger vers la case ou se trouve un poisson
+			// Si c'est un requin le déplacement est impossible
+			// Sinon on se déplace sur la case en mangeant la sardine
 		
 	}
 }
